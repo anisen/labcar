4eadec9a628d28d4d2d982e1f0de65bc7dbc11cd
“Anais <ananavelez@gmail.com>
2017-06-22 10:53:18 -0400

    encontrar en mapa listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,5 +12,38 @@ function initMap(){
 	var autocompletadoFinal= new google.maps.places.Autocomplete(document.getElementById("destino"));
 	autocompletadoFinal.bindTo("bounds", map);
 
+    function buscar(){
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
+	document.getElementById("trazar").addEventListener("click",buscar);
+	var latitud,longitud;
+
+	var funcionExito= function(posicion){
+		latitud= posicion.coords.latitude;
+		longitud= posicion.coords.longitude;
+
+		var miUbicacion= new google.maps.Marker({
+			position: {lat:latitud, lng:longitud},
+			animation: google.maps.Animation.DROP,
+			map:map,
+
+		});
+		map.setZoom(17);
+		map.setCenter({lat:latitud, lng:longitud});
+	}
+	var funcionError=function(error){
+		alert("Tenemos un problema con encontrar tu ubicacion");
+	};
+	var defaultBounds= new google.maps.LatLngBounds(
+		new google.maps.LatLng(-33.8902, 151.1759),
+		new google.maps.LatLng(-33.8474, 151.2631));
+
+	var options={
+		bounds:defaultBounds
+	};
+	var input=document.getElementById('pac-input');
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 }
